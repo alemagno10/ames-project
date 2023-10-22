49b4367a66537b09a35442a72c6b9b91d89c2732
alemagno10 <alemagno03@gmail.com>
2023-10-22 18:10:23 -0300

    api 1.2

--- api/ames_api.py
+++ api/ames_api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from utils import *
+import random
 
 app = Flask(__name__)
 
@@ -13,26 +14,25 @@ def hello_world():
 
 @app.route('/api', methods=['GET'])
 def teste():
-    return raw.iloc[10].to_dict(), 200
+    i = random.randint(0,2877)
+    return raw.iloc[i].to_dict(), 200
 
 @app.route('/api', methods=['POST'])
 def predict():
     data = request.get_json()
     df = pd.DataFrame([data])
 
-    if not check_fields(data, list(raw.columns)):
+    if not check_fields(data.keys(), list(raw.columns)):
         return jsonify({'error: Verifique os campos enviados'}), 400
     
-    predicted_y = model.predict(feature_engineering(df.iloc[0]))
-    print(predicted_y)
+    predicted_y = model.predict(feature_engineering(df))
 
     response = {
         'message': 'Regressão concluída com sucesso',
         'predicted_y': predicted_y.tolist()
     }
 
-    return df.T.to_dict(), 200
-    # return jsonify(response), 200
+    return jsonify(response), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
